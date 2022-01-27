bf53b71d616b895cc0acb8d224e14f57dddb572e
Whitsel <33960586+Whitsel@users.noreply.github.com>
2022-01-26 16:28:37 -0900

    Added C++ to fix crashing

---
+++ IPBC_R2C.VR/init.sqf
@@ -0,0 +1,1 @@
+execVM "shotsNear.sqf"
---
+++ IPBC_R2C.VR/script_macros.hpp
@@ -0,0 +1,69 @@
+
+#define QUOTE(var) #var
+#define DOUBLES(var1,var2) ##var1##_##var2
+#define TRIPLES(var1,var2,var3) ##var1##_##var2##_##var3
+
+#define MULT0(a)
+#define MULT1(a) a
+#define MULT2(a) MULT1(a), a
+#define MULT3(a) MULT2(a), a
+#define MULT4(a) MULT3(a), a
+#define MULT5(a) MULT4(a), a
+#define MULT6(a) MULT5(a), a
+#define MULT7(a) MULT6(a), a
+#define MULT8(a) MULT7(a), a
+#define MULT9(a) MULT8(a), a
+#define MULT10(a) MULT9(a), a
+#define MULT11(a) MULT10(a), a
+#define MULT12(a) MULT11(a), a
+#define MULT13(a) MULT12(a), a
+#define MULT14(a) MULT13(a), a
+#define MULT15(a) MULT14(a), a
+#define MULT16(a) MULT15(a), a
+#define MULT17(a) MULT16(a), a
+#define MULT18(a) MULT17(a), a
+#define MULT19(a) MULT18(a), a
+#define MULT20(a) MULT19(a), a
+
+#define ARR_1(ARG1) ARG1
+#define ARR_2(ARG1,ARG2) ARG1, ARG2
+#define ARR_3(ARG1,ARG2,ARG3) ARG1, ARG2, ARG3
+#define ARR_4(ARG1,ARG2,ARG3,ARG4) ARG1, ARG2, ARG3, ARG4
+#define ARR_5(ARG1,ARG2,ARG3,ARG4,ARG5) ARG1, ARG2, ARG3, ARG4, ARG5
+#define ARR_6(ARG1,ARG2,ARG3,ARG4,ARG5,ARG6) ARG1, ARG2, ARG3, ARG4, ARG5, ARG6
+#define ARR_7(ARG1,ARG2,ARG3,ARG4,ARG5,ARG6,ARG7) ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7
+#define ARR_8(ARG1,ARG2,ARG3,ARG4,ARG5,ARG6,ARG7,ARG8) ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7, ARG8
+
+#define GVAR(var1) DOUBLES(PREFIX,var1)
+#define EGVAR(var1,var2) TRIPLES(PREFIX,var1,var2)
+#define QGVAR(var1) QUOTE(GVAR(var1))
+#define QEGVAR(var1,var2) QUOTE(EGVAR(var1,var2))
+#define QQGVAR(var1) QUOTE(QGVAR(var1))
+#define QQEGVAR(var1,var2) QUOTE(QEGVAR(var1,var2))
+
+#define FUNC(var) TRIPLES(PREFIX,fnc,var)
+#define QFUNC(var) QUOTE(FUNC(var))
+#define EFUNC(var1,var2) FUNC(DOUBLES(var1,var2))
+#define QEFUNC(var1,var2) QUOTE(EFUNC(var1,var2))
+
+#define GETVAR_SYS(var1,var2) getVariable [ARR_2(QUOTE(var1),var2)]
+#define SETVAR_SYS(var1,var2) setVariable [ARR_2(QUOTE(var1),var2)]
+#define SETPVAR_SYS(var1,var2) setVariable [ARR_3(QUOTE(var1),var2,true)]
+
+#undef GETVAR
+#define GETVAR(var1,var2,var3) (var1 GETVAR_SYS(var2,var3))
+#define GETMVAR(var1,var2) (missionNamespace GETVAR_SYS(var1,var2))
+#define GETUVAR(var1,var2) (uiNamespace GETVAR_SYS(var1,var2))
+#define GETPRVAR(var1,var2) (profileNamespace GETVAR_SYS(var1,var2))
+#define GETPAVAR(var1,var2) (parsingNamespace GETVAR_SYS(var1,var2))
+
+#undef SETVAR
+#define SETVAR(var1,var2,var3) var1 SETVAR_SYS(var2,var3)
+#define SETPVAR(var1,var2,var3) var1 SETPVAR_SYS(var2,var3)
+#define SETMVAR(var1,var2) missionNamespace SETVAR_SYS(var1,var2)
+#define SETUVAR(var1,var2) uiNamespace SETVAR_SYS(var1,var2)
+#define SETPRVAR(var1,var2) profileNamespace SETVAR_SYS(var1,var2)
+#define SETPAVAR(var1,var2) parsingNamespace SETVAR_SYS(var1,var2)
+
+#define GETGVAR(var1,var2) GETMVAR(GVAR(var1),var2)
+#define GETEGVAR(var1,var2,var3) GETMVAR(EGVAR(var1,var2),var3)
